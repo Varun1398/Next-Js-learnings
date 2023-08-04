a294eeffe6522b1be9159a3aca2e078c26e670cd
varun <varun.sharma@easygov.co.in>
2023-08-04 12:51:23 +0530

    ssr-nextjs

---
+++ pages/news/[category].js
@@ -0,0 +1,39 @@
+export default function ArticleListByCategory({ articles, category }) {
+  return (
+    <>
+      <h1>
+        Showing news for category <i>{category}</i>
+      </h1>
+      {articles.map((article) => {
+        return (
+          <div key={article.id}>
+            <h2>
+              {article.id} {article.title}
+            </h2>
+            <p>{article.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params, req, res, query } = context;
+  console.log(req.headers.cookie);
+  console.log("query >>", query);
+
+  res.setHeader("Set-Cookie", ["name=varun"]);
+  const { category } = params;
+  const response = await fetch(
+    `http://localhost:4000/news?category=${category}`
+  );
+  const data = await response.json();
+  return {
+    props: {
+      articles: data,
+      category,
+    },
+  };
+}
