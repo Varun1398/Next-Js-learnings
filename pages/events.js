d9880d62f13d2724b98b4c658677cbc27c035e92
varun <varun.sharma@easygov.co.in>
2023-08-17 11:10:35 +0530

    nextjs-swr

---
+++ pages/events.js
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function EventList({ eventList }) {
+  const [events, setEvents] = useState(eventList);
+  const router = useRouter();
+  const fetchSportEvents = async () => {
+    const response = await fetch(
+      `http://localhost:4000/events?category=sports`
+    );
+    const data = await response.json();
+    setEvents(data);
+    router.push("/events?category=sports", undefined, { shallow: true });
+  };
+  return (
+    <>
+      <button onClick={fetchSportEvents}>Sport Events</button>
+      <h1>Event List</h1>
+      {events.map((event) => {
+        return (
+          <div key={event.id}>
+            <h2>
+              {event.id} {event.title} {event.data} | {event.category}
+            </h2>
+            <p>{event.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const { category } = query;
+  const queryString = category ? "category=sports" : "";
+  const response = await fetch(`http://localhost:4000/events?${queryString}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      eventList: data,
+    },
+  };
+}
